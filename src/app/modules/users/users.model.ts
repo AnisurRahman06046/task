bed489cd84480cae893dd18b77a5304e9fd0564d
AnisurRahman06046 <anisurrahman06046@gmail.com>
2023-12-25 14:21:33 +0600

    password hashing implemented using bcrypt

--- src/app/modules/users/users.model.ts
+++ src/app/modules/users/users.model.ts
@@ -1,6 +1,6 @@
 import { Schema, model } from "mongoose";
 import { TUSER } from "./users.interface";
-
+import bcrypt from "bcrypt";
 const userSchema = new Schema<TUSER>({
   fullName: {
     type: String,
@@ -14,6 +14,12 @@ const userSchema = new Schema<TUSER>({
     type: String,
     required: true,
   },
+});
+
+// hashing password before saving on database
+userSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 const User = model<TUSER>("User", userSchema);
