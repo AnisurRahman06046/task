468ed8c87948ba4d137491f966ee9e4757287e61
AnisurRahman06046 <anisurrahman06046@gmail.com>
2023-12-25 14:14:17 +0600

    global error handler created

---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response } from 'express';
+
+const globalErrorHanlders = (
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const statusCode = 500;
+  const message = error.message || 'Something went wrong';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    error: error,
+  });
+};
+
+export default globalErrorHanlders;
--- src/app/modules/users/users.controller.ts
+++ src/app/modules/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
 import catchAsync from "../../utils/catchAsync";
+import { userServices } from "./users.service";
+import sendResponse from "../../utils/sendApiResponse";
+import httpStatus from "http-status";
 
 const createUser = catchAsync(async (req: Request, res: Response) => {
-  
-    const data = req.body
-    
+  const data = req.body;
+  const result = await userServices.createUser(data);
+  sendResponse(res, httpStatus.OK, "User is registered successfully", result);
 });
 
 export const userControllers = { createUser };
