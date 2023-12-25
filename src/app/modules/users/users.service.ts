bcc17946890bb3800eb58a87cd21a4d0ca948200
AnisurRahman06046 <anisurrahman06046@gmail.com>
2023-12-25 21:01:11 +0600

    user create service modified

--- src/app/modules/users/users.service.ts
+++ src/app/modules/users/users.service.ts
@@ -1,10 +1,15 @@
+import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
 import { TUSER } from "./users.interface";
 import User from "./users.model";
 
-const createUser=async(payload:TUSER)=>{
-    const result = await User.create(payload)
-    return result
-}
+const createUser = async (payload: TUSER) => {
+  const user = await User.findOne({ email: payload.email });
+  if (user) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User is already exists");
+  }
+  const result = await User.create(payload);
+  return result;
+};
 
-
-export const userServices={createUser}
+export const userServices = { createUser };
