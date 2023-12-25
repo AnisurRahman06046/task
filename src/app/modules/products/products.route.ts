02b5a3a9a1b2c6d89e6690ce7d4f334ff09ad77e
AnisurRahman06046 <anisurrahman06046@gmail.com>
2023-12-25 22:50:59 +0600

    auth added on all products route

--- src/app/modules/products/products.route.ts
+++ src/app/modules/products/products.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { productControllers } from "./products.controller";
+import auth from "../../middlewares/auth";
 const router = Router();
 
 router.post("/create-product", productControllers.createProduct);
-router.get("/all-products", productControllers.getProducts);
+router.get("/all-products",auth(), productControllers.getProducts);
 export const productRoutes = router;
