96d1752b1e3c30a7e800e72e798502738a564043
AnisurRahman06046 <anisurrahman06046@gmail.com>
2023-12-25 20:32:59 +0600

    global error handler modified

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,19 +1,60 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { TErrorSource } from "../interfaces/error";
+import { ZodError } from "zod";
+import handleZodError from "../errors/handleZodError";
+import handleValidationError from "../errors/handleValidationError";
+import AppError from "../errors/AppError";
 
-const globalErrorHanlders = (
+const globalErrorHanlders: ErrorRequestHandler = (
   error: any,
   req: Request,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ) => {
-  const statusCode = 500;
-  const message = error.message || 'Something went wrong';
+  let statusCode = 500;
+  let message = error.message || "Something went wrong";
+  let errorSource: TErrorSource = [
+    {
+      path: "",
+      message: "Something went wrong",
+    },
+  ];
+
+  if (error instanceof ZodError) {
+    const err = handleZodError(error);
+    statusCode = err?.statusCode;
+    message = err?.message;
+    errorSource = err?.errorSource;
+  } else if (error?.name === "ValidationError") {
+    const err = handleValidationError(error);
+    statusCode = err?.statusCode;
+    message = err?.message;
+    errorSource = err?.errorSource;
+  } else if (error instanceof AppError) {
+    statusCode = error?.statusCode;
+    message = error?.message;
+    errorSource = [
+      {
+        path: "",
+        message: error?.message,
+      },
+    ];
+  } else if (error instanceof Error) {
+    message = error.message;
+    errorSource = [
+      {
+        path: "",
+        message: error?.message,
+      },
+    ];
+  }
   return res.status(statusCode).json({
     success: false,
     message,
     error: error,
+    errorSource,
   });
 };
 
